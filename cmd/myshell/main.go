e24953a7fdbda81d9e5c45541316fec428bcfb7f
Habibur Rahman <hbrawnak@gmail.com>
2024-12-27 00:53:57 +0600

    refactoring code

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -9,58 +9,71 @@ import (
 
 func main() {
 	for {
-		// Uncomment this block to pass the first stage
 		fmt.Fprint(os.Stdout, "$ ")
 
 		// Wait for user input
-		reader := bufio.NewReader(os.Stdin)
-		command, err := reader.ReadString('\n')
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
-
-			if err.Error() == "EOF" {
-				fmt.Fprintln(os.Stdout, "exit")
-				return
-			}
-
-			fmt.Fprintln(os.Stderr, "Error reading input:", err)
-			os.Exit(1)
+			handleError(err)
 		}
 
 		command = strings.TrimSpace(command)
+		if command == "" {
+			continue // Ignore empty commands
+		}
 
-		parseCommand(command)
+		executeCommand(command)
 	}
 }
 
-func parseCommand(command string) {
-	parts := strings.Fields(command) // Split the command into parts
-	if len(parts) == 0 {
+func executeCommand(input string) {
+	parts := strings.Fields(input)
+	command := parts[0]
+	args := parts[1:]
+
+	switch command {
+	case "exit":
+		handleExit(args)
+	case "echo":
+		handleEcho(args)
+	case "type":
+		handleType(args)
+	default:
+		fmt.Printf("%s: command not found\n", command)
+	}
+}
+
+func handleExit(args []string) {
+	if len(args) == 1 && args[0] == "0" {
+		os.Exit(0)
+	}
+	fmt.Println("Invalid exit command format")
+}
+
+func handleEcho(args []string) {
+	fmt.Println(strings.Join(args, " "))
+}
+
+func handleType(args []string) {
+	if len(args) != 1 {
+		fmt.Println("Usage: type <command>")
 		return
 	}
 
-	switch parts[0] {
-	case "exit":
-		if len(parts) > 1 && parts[1] == "0" {
-			os.Exit(0)
-		} else {
-			fmt.Println("Invalid exit command format")
-		}
-	case "echo":
-		fmt.Println(strings.Join(parts[1:], " "))
-	case "type":
-		parseType(parts[1])
+	switch args[0] {
+	case "echo", "type", "exit":
+		fmt.Printf("%s is a shell builtin\n", args[0])
 	default:
-		if command != "" {
-			fmt.Printf("%s: command not found\n", command)
-		}
+		fmt.Printf("%s: not found\n", args[0])
 	}
 }
 
-func parseType(typeStr string) {
-	switch typeStr {
-	case "echo", "type", "exit":
-		fmt.Printf("%s is a shell builtin\n", typeStr)
-	default:
-		fmt.Printf("%s: not found\n", typeStr)
+func handleError(err error) {
+	if err.Error() == "EOF" {
+		fmt.Fprintln(os.Stdout, "exit")
+		return
 	}
+
+	fmt.Fprintln(os.Stderr, "Error reading input:", err)
+	os.Exit(1)
 }
